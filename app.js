ced0e133bbd4be443783acd486f9f94b0d82c6c2
mateo <m.devia@uniandes.edu.co>
2019-09-11 19:16:34 -0500

    deploy try 15

--- app.js
+++ app.js
@@ -3,24 +3,20 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+
 var indexRouter = require("./routes/index");
 
 var app = express();
 
-
-
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
-
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "front/build")));
-
-
 
 app.get('*', function (_, res) {
   res.sendFile(path.join(__dirname, './front/build/index.html'), function (err) {
@@ -30,17 +26,12 @@ app.get('*', function (_, res) {
   })
 })
 
-
-
 app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
-
 });
-
-
 
 // error handler
 app.use(function (err, req, res, next) {
@@ -48,14 +39,8 @@ app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-
-
   // render the error page
   res.status(err.status || 500);
   res.render("error");
-
 });
-
-
-
 module.exports = app;
