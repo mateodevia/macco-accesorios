31fc295ca999b9ce738b1922851866ff3b89c52a
mateo <m.devia@uniandes.edu.co>
2019-09-12 20:14:17 -0500

    mongo try 7

--- app.js
+++ app.js
@@ -21,14 +21,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "front/build")));
 
-app.get('*', function (_, res) {
-  res.sendFile(path.join(__dirname, './front/build/index.html'), function (err) {
-    if (err) {
-      res.status(500).send(err)
-    }
-  })
-})
-
 app.use("/images", images);
 app.use("/crudBolsos", bolsos);
 
