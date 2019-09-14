d1a3a2902a6e2f8eb4219bf6ae410db154a81930
leonel naranjo <lf.naranjo11@uniandes.edu.co>
2019-09-14 14:23:51 -0500

    merge

--- front/src/components/catalogo/catalogo.jsx
+++ front/src/components/catalogo/catalogo.jsx
@@ -1,56 +1,157 @@
 import React, { Component } from "react";
 import "./catalogo.css";
 import PlainCard from "../plainCard/plainCard";
-import NoDisponible from "../noDisponible/noDisponible";
+import SideDrawer from "../sideDrawer/sideDrawer";
 
 class Catalogo extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = { uploading: false };
+
+    this.state = {
+      uploading: false,
+      productosAPintar: []
+    };
     this.handleClick = this.handleClick.bind(this);
-    this.handleClose = this.handleClose.bind(this);
-  }
-       
-  
-
-  handleClose() {
-    this.setState((state)=>{
-
-      return {uploading:false};
-    });
   }
 
-
-  handleClickToParent(){
-
-    this.props.handleClick=this.state.handleClick;
+  componentWillReceiveProps(newProps) {
+    this.setState({ productosAPintar: newProps.productos });
   }
+
   handleClick() {
-    this.setState((state)=>{
+    this.setState((state) => {
 
-      return {uploading:true};
+      return { uploading: true };
     });
   }
+
+  handlerFiltroTamaño = (tamaños) => {
+    let newState = { ...this.state };
+
+    let nuevaListaAPintar = this.props.productos.filter((producto) => {
+      if (!tamaños.pequeño && producto.tamaño === "pequeño") {
+
+        return false;
+      }
+      else if (!tamaños.mediano && producto.tamaño === "mediano") {
+
+        return false;
+      }
+      else if (!tamaños.grande && producto.tamaño === "grande") {
+
+        return false;
+      }
+      else {
+        return true;
+      }
+    });
+
+    newState.productosAPintar = nuevaListaAPintar;
+    this.setState(newState);
+  }
+
+  handlerFiltroPrecio = (precios) => {
+    let newState = { ...this.state };
+
+    let nuevaListaAPintar = this.props.productos.filter((producto) => {
+      if (!precios.precioBajo && (producto.precio <= 100000)) {
+
+        return false;
+      }
+      else if (!precios.precioMedio && (100000 <= producto.precio) && (producto.precio <= 200000)) {
+
+        return false;
+      }
+      else if (!precios.precioAlto && (200000 <= producto.precio)) {
+
+        return false;
+      }
+      else {
+        return true;
+      }
+    });
+
+    newState.productosAPintar = nuevaListaAPintar;
+    this.setState(newState);
+  }
+
+  handlerFiltroColor = (colores) => {
+    let newState = { ...this.state };
+
+    let nuevaListaAPintar = this.props.productos.filter((producto) => {
+      if (!colores.negro && producto.color === "negro") {
+
+        return false;
+      }
+      else if (!colores.azul && producto.color === "azul") {
+
+        return false;
+      }
+      else if (!colores.cafe && producto.color === "cafe") {
+
+        return false;
+      }
+      else if (!colores.avano && producto.color === "avano") {
+
+        return false;
+      }
+      else if (!colores.blanco && producto.color === "blanco") {
+
+        return false;
+      }
+      else if (!colores.azulClaro && producto.color === "azulClaro") {
+
+        return false;
+      }
+      else if (!colores.verde && producto.color === "verde") {
+
+        return false;
+      }
+      else if (!colores.amarillo && producto.color === "amarillo") {
+
+        return false;
+      }
+      else if (!colores.rojo && producto.color === "rojo") {
+
+        return false;
+      }
+      else if (!colores.fucsia && producto.color === "fucsia") {
+
+        return false;
+      }
+      else if (!colores.rosado && producto.color === "rosado") {
+
+        return false;
+      }
+      else {
+        return true;
+      }
+    });
+
+    newState.productosAPintar = nuevaListaAPintar;
+    this.setState(newState);
+  }
+
   render() {
     return (
- <div>
-       {this.state.uploading &&   <div className="modal" id="myModal">
-       <div className="modal-content">
-    <button className="close" onClick={this.handleClose}>x</button>
-          <p>Some text in the Modal..</p>
-          </div>
-        </div>}
+      <div>
+        <SideDrawer handlerFiltroTamano={this.handlerFiltroTamaño} handlerFiltroPrecio={this.handlerFiltroPrecio} handlerFiltroColor={this.handlerFiltroColor}></SideDrawer>
         <div className="row justify-content-center">
-          {this.props.productos.map(
+          {this.state.productosAPintar.map(
             producto =>
               < PlainCard autenticado={this.props.autenticado} key={producto._id} rutaImagen={producto.imagen} nombre={producto.nombre} precio={producto.precio} />
           )}
         </div>
         <button onClick={this.handleClick}>heyyyyyyy</button>
+        {this.state.uploading &&
+          <div id="modal"> <div className="modalContent">    Lorem, ipsum dolor sit a earum explicabo, tentiuepellat porro dolor.
+          </div>
+          </div>
+        }
+
       </div>
     );
   }
 }
-
 export default Catalogo;
