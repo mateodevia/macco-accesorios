f85e883c183e94b9bbda1d368b793252f2c01a7a
mateo <m.devia@uniandes.edu.co>
2019-09-14 22:37:51 -0500

    delete item done

--- front/src/components/bolsosPage/bolsosPage.jsx
+++ front/src/components/bolsosPage/bolsosPage.jsx
@@ -26,13 +26,27 @@ class BolsosPage extends Component {
     });
     promesa.catch(() => console.log("pailas"));
   }
+
+  refresh = () => {
+    let promesa = axios.get("/crudBolsos", {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      }
+    });
+    promesa.then(res => {
+
+      this.setState({ productos: res.data });
+
+    });
+    promesa.catch(() => console.log("pailas"));
+  }
   render() {
     return (
       <React.Fragment>
         <div>
           <img className="imagen" src="https://res.cloudinary.com/drfggfn8f/image/upload/v1568479748/macco/bolsos/kbm6hsxlel13qoijtr9v.jpg" />
         </div>
-        <Catalogo productos={this.state.productos} autenticado={this.props.autenticado} filtro={true} />
+        <Catalogo productos={this.state.productos} autenticado={this.props.autenticado} filtro={true} refresh={this.refresh} />
       </React.Fragment>
     );
   }
